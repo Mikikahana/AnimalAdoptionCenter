f48e1d04d46132c6243de3b47bfbfab77352ba54
michoel kahana <kmichoel25@gmail.com>
2023-01-24 10:40:47 -0500

    Finished up components

--- animaladoptioncenter/src/App.js
+++ animaladoptioncenter/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react"
+import DogContainer from './DogContainer';
+import Header from './Header';
+import HomePage from './HomePage'
+function App()
+{
+  const [pets, setPets] = useState([])
+  useEffect(function ()
+  {
+    fetch("http://localhost:3000/pets")
+      .then(function (resp)
+      {
+        return resp.json()
+      })
+      .then(function (data)
+      {console.log(data)
+        return setPets(data)
+        
+      })
+  }, [])
+  function handleUpdatedPets(updatedPet)
+  {
+    const updatedPetList = pets.map(function (pet)
+    {
+      if (pet.id === updatedPet.id) {
+        return updatedPet
+      }
+      else {
+        return pet
+      }
+    })
+    setPets(updatedPetList)
+  }
+  const [searchInput, setSearchInput] = useState("")
+  function handleSearchInput(e)
+  {
+    setSearchInput(e.target.value)
+  }
+  const filterDogs = pets.filter(function (pet)
+  {
+    return pet.name.toLowerCase().includes(searchInput.toLowerCase())
+  })
+  function handleNewDog(newDog)
+  {
+    setPets([...pets, newDog])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header handleSearchInput={handleSearchInput} handleNewDog={handleNewDog} />
+      <HomePage />
+      <DogContainer pets={filterDogs} handleUpdatedPets={handleUpdatedPets} />
     </div>
   );
 }
+export default App
 
-export default App;
+
+
+
+
+
+
+
+
